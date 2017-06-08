bb6fa942eb6eef15190e0c5e39aa9cbab1d29697
John Goodman <johncurtisgoodman@gmail.com>
2017-06-08 01:56:12 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,91 @@
+var position = {
+  accel: 0,
+  x: 0,
+};
+
+var lost = false;
+var won = false;
+var starttime = 0;
+
+var startgame = function () {
+  var error;
+  var meter;
+  var needle;
+  var start;
+  var video;
+  lost = false;
+  won = false;
+  error = document.getElementsByClassName('error-video')[0];
+  meter = document.getElementsByClassName('meter')[0];
+  needle = document.getElementsByClassName('needle')[0];
+  start = document.getElementsByClassName('start')[0];
+  video = document.getElementsByClassName('video')[0];
+  start.innerText = "Mash the left and right arrow keys.";
+  start.onclick = null;
+  video.play();
+  needle.x = Math.random() - 0.5;
+  position.x = 0;
+  starttime = new Date().getTime() / 1000;
+  interval = window.setInterval(function () {
+    var needle;
+    var counter;
+    needle = document.getElementsByClassName('needle')[0];
+    counter = document.getElementsByClassName('counter')[0];
+    if (position.x > 0) {
+      position.accel -= Math.random() * 5 - 2;
+    } else {
+      position.accel += Math.random() * 5 - 2;
+    }
+    if (position.accel > 8) { position.accel = 8; }
+    if (position.accel < -8) { position.accel = -8; }
+    position.x += position.accel;
+    if (position.x > 50 || position.x < -50) {
+      lose();
+    }
+    needle.style.transform = 'translateX(' + position.x + 'px)';
+    counter.innerText = Math.round(new Date().getTime() / 100 - starttime * 10) / 10;
+  }, 80);
+};
+
+onkeydown = function (event) {
+  if (event.keyCode === 37) { // left
+    position.x -= 8;
+  } else if (event.keyCode === 39) { // right
+    position.x += 8;
+  }
+};
+
+var lose = function () {
+  var error;
+  var start;
+  var video;
+  if (lost) { return false; }
+  lost = true;
+  error = document.getElementsByClassName('error-video')[0];
+  start = document.getElementsByClassName('start')[0];
+  video = document.getElementsByClassName('video')[0];
+  video.style.display = 'none';
+  error.style.display = 'block';
+  if (!won) {
+    start.onclick = startgame;
+    start.innerText = "You're embarrassed in front of everyone. Try again?";
+  }
+  video.currentTime = 0;
+  video.pause();
+  error.play();
+  window.clearInterval(interval);
+};
+
+var win = function () {
+  var music;
+  var start;
+  won = true;
+  music = document.getElementById('victory');
+  start = document.getElementsByClassName('start')[0];
+  start.innerText = "You made it through the meeting without embarrassing yourself in front of Count Dooku!";
+  music.play();
+};
+
+window.onload = function () {
+  document.getElementsByClassName('video')[0].addEventListener('ended', win, false);
+};
